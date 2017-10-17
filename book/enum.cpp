193e1b2aefa5d684c4d58aed3f23a536fca78fa7
Jang Woo Park <jangpark@g.ucla.edu>
2017-10-17 17:02:01 +0000

    Add files
    files from chapter 6

--- book/enum.cpp
+++ book/enum.cpp
@@ -1,16 +1,29 @@
-// enum.cpp -- a simple enumeration
+// enum.cpp -- use enum
 #include <iostream>
-#include <cstring>
-using namespace	std;
-enum spectrum {red, orange, yellow, green, blue, violet, indigo, ultraviolet};
+// create named constants for 0 - 6
+enum {red, orange, yellow, green, blue, violet, indigo};
 
-int	main()
+int main()
 {
-	enum spectrum a, b,c;
-	int d;
-	a = orange;
-	b = spectrum(5);
-	d = orange + red;
-	cout << d << endl;
-	return 0;
+    using namespace std;
+    cout << "Enter color code (0-6): ";
+    int code;
+    cin >> code;
+    while (code >= red && code <= indigo)
+    {
+        switch (code)
+        {
+            case red     : cout << "Her lips were red.\n"; break;
+            case orange  : cout << "Her hair was orange.\n"; break;
+            case yellow  : cout << "Her shoes were yellow.\n"; break;
+            case green   : cout << "Her nails were green.\n"; break;
+            case blue    : cout << "Her sweatsuit was blue.\n"; break;
+            case violet  : cout << "Her eyes were violet.\n"; break;
+            case indigo  : cout << "Her mood was indigo.\n"; break;
+        }
+        cout << "Enter color code (0-6): ";
+        cin >> code;
+    }
+    cout << "Bye\n";
+    return 0; 
 }
