c8f92c3d38409338e9d016488c724cf8cbcb20f4
Jang Woo Park <jangpark@g.ucla.edu>
2017-11-02 18:56:00 +0000

    files: upload
    ch 15

--- book/nested.cpp
+++ book/nested.cpp
@@ -1,35 +1,32 @@
-// nested.cpp -- nested loops and 2-D array
+// nested.cpp -- using a queue that has a nested class
 #include <iostream>
-const int Cities = 5;
-const int Years = 4;
+
+#include <string>
+#include "queuetp.h"
+
 int main()
 {
-    using namespace std;
-    const char * cities[Cities] =   // array of pointers
-    {                               // to 5 strings
-        "Gribble City",
-        "Gribbletown",
-        "New Gribble",
-        "San Gribble",
-        "Gribble Vista"
-    };
+    using std::string;
+    using std::cin;
+    using std::cout;
 
-    int maxtemps[Years][Cities] =   // 2-D array
-    {
-        {95, 99, 86, 100, 104},   // values for maxtemps[0]
-        {95, 97, 90, 106, 102},   // values for maxtemps[1]
-        {96, 100, 940, 107, 105}, // values for maxtemps[2]
-        {97, 102, 89, 108, 104}   // values for maxtemps[3]
-    };
+    QueueTP<string> cs(5);
+    string temp;
 
-    cout << "Maximum temperatures for 2002 - 2005\n\n";
-    for (int city = 0; city < Cities; ++city)
+    while(!cs.isfull())
     {
-        cout << cities[city] << ":\t";
-        for (int year = 0; year < Years; ++year)
-            cout << maxtemps[year][city] << "\t";
-        cout << endl;
+        cout << "Please enter your name. You will be "
+                "served in the order of arrival.\n"
+                "name: ";
+        getline(cin, temp);
+        cs.enqueue(temp);
     }
+    cout << "The queue is full. Processing begins!\n";
 
-    return 0;
+    while (!cs.isempty())
+    {
+        cs.dequeue(temp);
+        cout << "Now processing " << temp << "...\n";
+    }
+    return 0; 
 }
